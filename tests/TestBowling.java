0a2262cb7df2f98df597240558a3d5f67b50abdf
somename <someemail>
2017-05-11 20:20:59 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -8,5 +8,14 @@ public class TestBowling {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void test_Spare() {
+		Frame frame = new Frame(5,5);
+		boolean isSpare = frame.isSpare();
+		
+		assertEquals("Incorrect", true, isSpare);
+		
+	}
 
 }
